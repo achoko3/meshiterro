b146cc0a269a3ceeb65b4d4c17315cc7c1d84960
Aya Yamamoto <132640547+achoko3@users.noreply.github.com>
2023-06-25 05:17:08 +0000

    [Finish]4-15

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :post_images, dependent: :destroy
 
 
-  has_one_attaches :profile_image
+  has_one_attached :profile_image
 
 
   def get_profile_image(width, height)
